8c1137deb948b7e54bd00a16191e18cc40c1d51a
Jaied Ali Juned <70015907+Zeaid-Ali-Ghori@users.noreply.github.com>
2020-09-05 17:27:29 +0530

    Day 4- Assignment 1

---
+++ Day 4-Armstrong number.py.py
@@ -0,0 +1,15 @@
+# Program to check Armstrong numbers
+
+lower = 1042000
+upper = 702648265
+for num in range(lower, upper + 1):
+    order = len(str(num))
+    sum = 0
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    if num == sum:
+        print("The first Armstrong Number is",num)
+        break
